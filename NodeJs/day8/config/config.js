1f6470ae72f4e2b89de900cad472fc59c4e719a6
binna <every5116@naver.com>
2021-04-20 11:57:38 +0900

    2021.03.14

--- NodeJs/day8/config/config.js
+++ NodeJs/day8/config/config.js
@@ -3,8 +3,8 @@ module.exports = {
     db_url: 'mongodb://localhost:27017/frontenddb',
     db_schemas: [{file:'./member_schema', collection:'member2', schemaName:'MemberSchema', modelName:'MemberModel'}],
     facebook: {
-        clientID: '112455474192330',
-        clientSecret: 'b7bda3904fc3aa454cee165e581dd44d',
+        clientID: '앱 ID',
+        clientSecret: '앱 시크릿 코드',
         callbackURL: 'http://localhost:3000/auth/facebook/callback'
     }
 }
