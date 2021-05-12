d1401234b19fdd4eb2e351a8e0c4e8df8765b113
binna <every5116@naver.com>
2021-05-12 11:43:47 +0900

    2021.03.20

---
+++ React/day1/begin/src/App.js
@@ -0,0 +1,15 @@
+import React from 'react';
+import Hello from './Hello';
+
+function App() {
+  return (
+    <>
+      <Hello/>
+      <Hello/>
+      <Hello/>
+      <Hello/>
+    </>
+  );
+}
+
+export default App;
