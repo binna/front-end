f06952dcb8e1fbcda94bd24d6f3b34c3edaaab2a
binna <every5116@naver.com>
2021-05-16 13:39:03 +0900

    2021.03.28

--- React/Day3/with-css/src/app.jsx
+++ React/Day3/with-css/src/app.jsx
@@ -6,7 +6,7 @@ function App() {
   return (
     <div className="App">
       <div className="height">
-        <Button size="large">버튼 1</Button>
+        <Button size="large" onClick={() => console.log('클릭!')}>버튼 1</Button>
         <Button>버튼 2</Button>
         <Button size="small">버튼 3</Button>
       </div>
@@ -21,6 +21,18 @@ function App() {
         <Button size="large" color="pink">버튼 7</Button>
         <Button color="pink">버튼 8</Button>
         <Button size="small" color="pink">버튼 9</Button>
+      </div>
+
+      <div className="height">
+        <Button size="large" color="blue" outline>버튼 10</Button>
+        <Button color="gray" outline>버튼 11</Button>
+        <Button size="small" color="pink" outline>버튼 12</Button>
+      </div>
+
+      <div className="height">
+        <Button size="large" color="blue" fullWidth>버튼 10</Button>
+        <Button color="gray" fullWidth>버튼 11</Button>
+        <Button size="small" color="pink" fullWidth>버튼 12</Button>
       </div>
     </div>
   );
