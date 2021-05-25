17913afffc0f1338fba06b792dca502798cf869f
binna <every5116@naver.com>
2021-05-25 15:51:58 +0900

    2021.04.04

--- React/Day5/router/src/app.jsx
+++ React/Day5/router/src/app.jsx
@@ -1,13 +1,34 @@
 import React from 'react'
-import {Route} from 'react-router-dom';
+import {Route, Link} from 'react-router-dom';
 import Home from './home';
 import About from './about';
+import Profiles from './profiles';
+import HistorySample from './historySample';
 
 const App = () => {
   return (
     <div>
-      <Route path="/" exact={true} component={Home}/>
-      <Route path="/about" component={About}/>
+      <ul>
+        <li>
+          <Link to='/'>홈</Link>
+        </li>
+        <li>
+          <Link to="/about">소개</Link>
+        </li>
+        <li>
+          <Link to="/profiles">프로필 목록</Link>
+        </li>
+        <li>
+          <Link to="/history">history 예제</Link>
+        </li>
+      </ul>
+      <hr/>
+      <div>
+        <Route path="/" exact={true} component={Home}/>
+        <Route path="/about" component={About}/>
+        <Route path="/profiles" component={Profiles}/>
+        <Route path="/history" component={HistorySample}/>
+      </div>
     </div>
   )
 }
